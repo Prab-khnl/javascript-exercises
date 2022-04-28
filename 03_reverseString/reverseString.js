371be114489e1046925cb61ad953619bac6357d5
Prabesh Khanal <prabkhnl@gmail.com>
2022-04-28 09:28:41 +0545

    Completed exercise 1 and 2

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
 const reverseString = function() {
-
-};
+    let rev = "";
+    let word = "hello";
+    for (let i = 0 ; i < rev.length; i++) {
+            rev.unshift(word[i]);
+    }
+ };
 
 // Do not edit below this line
 module.exports = reverseString;
